2abf9f4688e5c35e04b007a071d5a8faaaef836c
mathimarques <mathias.marques@outlook.com>
2022-01-07 13:23:18 -0300

    Dashboard total usuarios categoria y productos

--- src/components/ContentRowInfo.js
+++ src/components/ContentRowInfo.js
@@ -1,50 +1,74 @@
-import React from 'react';
-import SmallCard from './SmallCard';
+import React, { useState, useEffect } from "react";
+import SmallCard from "./SmallCard";
 
-/*  Cada set de datos es un objeto literal */
+function ContentRowInfo() {
+  const [products, setProducts] = useState(0);
+  const [users, setUsers] = useState(0);
 
-/* <!-- Movies in DB --> */
+  /*  Cada set de datos es un objeto literal */
 
-let moviesInDB = {
-    title: 'Movies in Data Base',
-    color: 'primary', 
-    cuantity: 21,
-    icon: 'fa-clipboard-list'
-}
+  /* <!-- Users in DB --> */
 
-/* <!-- Total awards --> */
+  let usersInDB = {
+    title: "Total de usuarios",
+    color: "primary",
+    cuantity: users.total,
+    icon: "fa-user-check",
+  };
 
-let totalAwards = {
-    title:' Total awards', 
-    color:'success', 
-    cuantity: '79',
-    icon:'fa-award'
-}
+  /* <!-- Products in DB --> */
 
-/* <!-- Actors quantity --> */
+  let productsInDB = {
+    title: "Total de productos",
+    color: "warning",
+    cuantity: products.total,
+    icon: "fa-clipboard-list",
+  };
 
-let actorsQuantity = {
-    title:'Actors quantity' ,
-    color:'warning',
-    cuantity:'49',
-    icon:'fa-user-check'
-}
+  /* <!-- Categories in DB --> */
 
-let cartProps = [moviesInDB, totalAwards, actorsQuantity];
+  let totalCategories = {
+    title: "Total de categorías",
+    color: "success",
+    cuantity: products.countByCategory,
+    icon: "fa-award",
+  };
 
-function ContentRowInfo(){
-    return (
-    
-        <div className="row">
-            
-            {cartProps.map( (movie, i) => {
+  /* <!-- Actors quantity --> */
 
-                return <SmallCard {...movie} key={i}/>
-            
-            })}
+  let cartProps = [usersInDB, productsInDB, totalCategories];
 
-        </div>
-    )
+  useEffect(() => {
+    fetch("/api/products/")
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.data !== undefined ){
+          setProducts(data.meta);
+        } else {
+          setProducts([]);
+        }
+      })
+      .catch((error) => console.log(error));
+
+    fetch("/api/users/")
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.data !== undefined ){
+            setUsers(data.meta);
+          } else {
+            setUsers([]);
+          }
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <div className="row">
+      {cartProps.map((data, i) => {
+        return <SmallCard {...data} key={i} />;
+      })}
+    </div>
+  );
 }
 
 export default ContentRowInfo;
--- src/components/SearchProducts.js
+++ src/components/SearchProducts.js
@@ -1,95 +1,87 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from "react";
 
-import noPoster from '../assets/images/no-poster.jpg';
+function SearchProducts() {
+  const [products, setProducts] = useState([]);
+  const [keyword, setKeyword] = useState([]);
 
-function SearchProducts(){
-	const [products, setProducts] = useState([]);
-	const [keyword, setKeyword] = useState('Piano');
+  const inputTag = useRef();
 
-	const inputTag = useRef();
+  useEffect(() => {
+    // Petición Asincrónica al montarse el componente
+    // const endpoint = `http://localhost:3000/api/products/?s=${keyword}`;
+    const endpoint = `/api/products/${keyword}`;
 
-	// Credenciales de API
-	// const apiKey = 'e24ea09d';
-	
-	useEffect(() => {
-		// Petición Asincrónica al montarse el componente
-		const endpoint = `http://localhost:3000/api/products/?s=${keyword}`;
+    fetch(endpoint)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        if (data.data !== undefined) {
+          setProducts(data.data);
+        } else {
+          setProducts([]);
+        }
+      })
+      .catch((error) => console.log(error));
+    // }
+  }, [keyword]);
 
-		// if (apiKey !== '') {
-			fetch(endpoint)
-				.then(response => response.json())
-				.then(data => {
-					if (!data.Error) {
-						setProducts(data.Search);
-					} else {
-						setProducts([]);
-					}
-				})
-				.catch(error => console.log(error))
-		// }
-	}, [keyword])
+  const searchProduct = async (e) => {
+    e.preventDefault();
+    const inputValue = inputTag.current.value;
+    setKeyword(inputValue);
+    inputTag.current.value = "";
+  };
 
-	const searchProduct = async e => {
-		e.preventDefault();
-		const inputValue = inputTag.current.value;
-		setKeyword(inputValue);
-		inputTag.current.value = '';
-	}
-
-	return(
-		<div className="container-fluid">
-			{
-				// apiKey !== '' ?
-				<>
-					<div className="row my-4">
-						<div className="col-12 col-md-6">
-							{/* Buscador */}
-							<form method="GET" onSubmit={searchProduct}>
-								<div className="form-group">
-									<label htmlFor="">Buscar por producto:</label>
-									<input ref={inputTag} type="text" className="form-control" />
-								</div>
-								<button className="btn btn-info">Buscar</button>
-							</form>
-						</div>
-					</div>
-					<div className="row">
-						<div className="col-12">
-							<h2>Instrumentos {keyword}</h2>
-						</div>
-						{/* Listado de películas */}
-						{
-							products.length > 0 && products.map((product, i) => {
-								return (
-									<div className="col-sm-6 col-md-3 my-4" key={i}>
-										<div className="card shadow mb-4">
-											<div className="card-header py-3">
-												<h5 className="m-0 font-weight-bold text-gray-800">{product.Title}</h5>
-											</div>
-											<div className="card-body">
-												<div className="text-center">
-													<img 
-														className="img-fluid px-3 px-sm-4 mt-3 mb-4" 
-														src={product.Poster !== 'N/A' ? product.Poster : noPoster}
-														alt={product.Title} 
-														style={{ width: '90%', height: '400px', objectFit: 'cover' }} 
-													/>
-												</div>
-												<p>{product.Year}</p>
-											</div>
-										</div>
-									</div>
-								)
-							})
-						}
-					</div>
-					{ products.length === 0 && <div className="alert alert-warning text-center">No se encontraron Instrumentos</div>}
-				</>
-				// :
-				// <div className="alert alert-danger text-center my-4 fs-2">Eyyyy... ¿PUSISTE TU APIKEY?</div>
-			}
-		</div>
-	)
+  return (
+    <div className="container-fluid">
+      {
+        <>
+          <div className="row my-4">
+            <div className="col-12 col-md-6">
+              {/* Buscador */}
+              <form method="GET" onSubmit={searchProduct}>
+                <div className="form-group">
+                  <label htmlFor="">Buscar por producto:</label>
+                  <input ref={inputTag} type="text" className="form-control" />
+                </div>
+                <button className="btn btn-info">Buscar</button>
+              </form>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-12">
+              <h2>Instrumentos {keyword}</h2>
+            </div>
+            {/* Listado de productos */}
+            {products.length > 0 &&
+              products.map((product, i) => {
+                return (
+                  <div className="col-sm-6 col-md-3 my-4" key={i}>
+                    <div className="card shadow mb-4">
+                      <div className="card-header py-3">
+                        <h5 className="m-0 font-weight-bold text-gray-800">
+                          {product.id}
+                        </h5>
+                      </div>
+                      <div className="card-body">
+                        <div className="text-center">
+                          <p>{product.model}</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+          </div>
+          {products.length === 0 && (
+            <div className="alert alert-warning text-center">
+              No se encontraron Instrumentos
+            </div>
+          )}
+        </>
+      }
+    </div>
+  );
 }
 
 export default SearchProducts;

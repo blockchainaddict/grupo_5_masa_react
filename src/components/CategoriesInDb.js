8a67306d484a6c5a856a9ef4c0ee351fcdcb8872
mathimarques <mathias.marques@outlook.com>
2022-01-09 14:22:23 -0300

    Panel de categorias

--- src/components/CategoriesInDb.js
+++ src/components/CategoriesInDb.js
@@ -1,39 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ContentRowCategories from "./ContentRowCategories";
 
 function CategoriesInDb() {
+  const [products, setProducts] = useState([]);
+
+  let typeArray = [];
+  let categoriesArray = [];
+
+  useEffect(() => {
+    fetch("/api/products/byCategories")
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.data !== undefined) {
+          setProducts(data.data);
+        } else {
+          setProducts([]);
+        }
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  products.map((product) => {
+    if (product !== undefined) {
+      return typeArray.push(product.type);
+    } else return products;
+  });
+
+  categoriesArray = [...new Set(typeArray.map((category) => category.name))];
+
+  console.log(categoriesArray);
+  if (categoriesArray.length === 0) {
+    categoriesArray = ['N/A'];
+  }
+
+
   return (
-    <div className="col-lg-6 mb-4">
-      <div className="card shadow mb-4">
-        <div className="card-header py-3">
-          <h5 className="m-0 font-weight-bold text-gray-800">
-            Tipos de Instrumentos musicales
-          </h5>
-        </div>
-        <div className="card-body">
-          <div className="row">
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Teclados</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Percusión</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Vientos</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Cuerdas</div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div className="row">
+      <ContentRowCategories categories={categoriesArray} />
     </div>
   );
 }
---
+++ src/components/ContentRowCategories.js
@@ -0,0 +1,54 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function ContentRowCategories(props) {
+  let listadoDeCategorias;
+  if (props.categories !== null) {
+    listadoDeCategorias = (
+      <div className="card-body">
+        {props.categories.map((category, i) => (
+          <div className="row" key={category + i}>
+            <div className="col-lg-12 mb-4">
+              <div className="card bg-dark text-white shadow">
+                <div className="card-body">{category}</div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  } else {
+    listadoDeCategorias = "";
+  }
+
+  return (
+    <div className="col-lg-12 mb-4">
+      <div className="card shadow mb-4">
+        <div className="card-header py-3">
+          <h5 className="m-0 font-weight-bold text-gray-800">
+            Tipos de Instrumentos musicales
+          </h5>
+        </div>
+        {listadoDeCategorias}
+      </div>
+    </div>
+  );
+}
+
+/* DEFINICIÓN DE PROPIEDADES POR DEFAULT */
+
+ContentRowCategories.defaultProps = {
+  categories: "N/A",
+  cuantity: "N/A",
+};
+
+/* PROPTYPES */
+
+ContentRowCategories.propTypes = {
+  atritutes: PropTypes.shape({
+    categories: PropTypes.string.isRequired,
+    cuantity: PropTypes.number.isRequired,
+  }),
+};
+
+export default ContentRowCategories;
